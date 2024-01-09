2633317f065b88bda84b27bf1714fac3252bab67
FILIPE RIBEIRO DE SOUZA <filipe.ribeiro@SETELAGOAS.MG>
2024-01-09 15:19:26 -0300

    exercicio 1 aula 12

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -1,1 +1,27 @@
-window.alert('Olá!')
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imgmanha')
+    var data = new Date()
+    var hora = data.getHours()
+    //var hora = 10
+    var minuto = data.getMinutes()
+    msg.innerHTML = `Agora são ${hora} horasd e ${minuto} minutos,`
+
+    if (hora >= 0 && hora < 12){
+        //BOM DIA!
+        img.src = 'ftmanha.jpg'
+        document.body.style.background = '#E2CD9F'
+        msg.innerHTML += ' Bom dia!'
+    } else if (hora >= 12 && hora <= 18) {
+        //BOA TARDE!
+        img.src ='fttarde.jpg'
+        document.body.style.background = '#e65c2e'
+        msg.innerHTML += ' Boa tarde!'
+    } else {
+        img.src = 'ftnoite.jpg'
+        document.body.style.background = '#1a1a1f'
+        msg.innerHTML += ' Boa noite!'
+        //BOA NOITE!
+    }
+}
+
